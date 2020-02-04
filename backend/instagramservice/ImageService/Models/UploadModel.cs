65bb51af253e651d70e48b8996eb3187221b5807
mars-boy <akhilvantipalli@gmail.com>
2020-02-04 08:34:16 +0530

    added missing model for upload

---
+++ backend/instagramservice/ImageService/Models/UploadModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ImageService.Models
+{
+    public class UploadModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public virtual IList<string> Tags { get {
+            List<string> tags = new List<string>();
+            if (!string.IsNullOrEmpty(Description)) {
+                var regex = new Regex(@"#\w+");
+                var tg = regex.Matches(Description);
+                if (tg != null && tg.Any())
+                {
+                    tags.AddRange(tg.Select(x => x.Value));
+                }
+            }
+            return tags;
+        } }
+    }
+}
